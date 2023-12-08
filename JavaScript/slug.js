3ce0e72964b6c4afa9109441623906730b079711
Kye Gorosave <kye.goro@icloud.com>
2023-12-08 01:52:15 -0800

    Slug Clicker Changes
    added a while bunch of stuff like speed slider, game rules, logo, different slugs, different points

--- JavaScript/slug.js
+++ JavaScript/slug.js
@@ -1,20 +1,33 @@
 /*
-    slug.js(!PROTOTTPE!) - This JavaScript files contains all the goods for how thr slug clicker works.
-    (Slugs falliing, click interactivity, starting the game, ending the game, updating the timer
-    updating the score, displaying your score, and ofc more slug stuff.)
-    
+    slug.js - This JavaScript file contains all the functionality for the Slug Clicker game.
     Requirements: jQuery must be loaded for this script to work.
-
     Authors: Kye Gorosave, Corey Haro, Leyton Philbrook, Parker Ehlers
-    
     Date: November 26, 2023
 */
 
 $(document).ready(function() {
+    //Defining variables and constants---------------------------------------------------------------------------
     const gameArea = $('#game-area');
-    let score = 0, timeLeft = 30, gameInterval, slugSpeed = 7;
+    let score = 0, timeLeft = 30, gameInterval;
 
-    // Function to create the initial game UI
+    const bananaSlugFacts = [
+        //Array of slug facts!---------------------------------------------------------------------------
+        "California's banana slug holds the title of the world's second-largest slug, surpassed only by the European Limax cinereoniger!",
+        "Banana slugs can reach lengths of up to 10 inches!",
+        "With a leisurely pace of about 7.5 inches per minute, banana slugs stand as one of the slowest creatures on the planet!",
+        "Weighing in at over 4 ounces, banana slugs are no lightweight in the slug world!",
+        "The color of banana slugs can vary based on factors such as age, diet, exposure to light and the elements, as well as the moisture content in the atmosphere!",
+        "Functioning as detritivores, banana slugs play a vital role in processing leaves, animal droppings, moss, and deceased plant materials, converting them into nutrient-rich soil humus!",
+        "While folklore may suggest that picking up and kissing slugs brings good fortune, it's advisable to refrain from doing so. The slimy coating on slugs aids in breathing and serves as protection against predators!",
+        "Contrary to popular belief, UCSC T-shirts and signs claiming 'No known predators' for the banana slug are inaccurate. These slugs have various predators, including salamanders, newts, and porcupines!",
+        "Featured in the movie Pulp Fiction, there is a UCSC banana slug shirt. Director Quentin Tarantino, a frequent visitor to Santa Cruz, specifically requested the student-designed Fiat Slug shirt for the film!",
+        "Banana slugs sport three openings on the right side of their heads, one of which is dedicated to breathing!",
+        "As hermaphrodites, banana slugs possess both male and female genitals, allowing them to self-fertilize when the need arises!",
+        "Meet the 'Banana Slug String Band,' a musical group with a unique name inspired by these fascinating creatures!",
+        "The slime of a banana slug contains chemicals that act as an anesthetic, numbing the tongue and throat of any unfortunate animal attempting to consume it!",
+        "Banana slugs employ pheromones in their slime to attract potential mates!"
+    ]
+    // Function to create the game UI---------------------------------------------------------------------------
     function createGameUI() {
         gameArea.empty();
         gameArea.append(`
@@ -24,6 +37,7 @@ $(document).ready(function() {
             </div>
             <button id="start-button">Start Game</button>
         `);
+        //Adding a timer and a start button---------------------------------------------------------------------------
         $('#start-button').click(startGame).css({
             position: 'absolute',
             top: '50%',
@@ -34,9 +48,9 @@ $(document).ready(function() {
         });
     }
 
-    // Function to start the game
+    // Function to start the game---------------------------------------------------------------------------
     function startGame() {
-        $('#end-game-button').show(); // Show the end game button when a new game starts
+        $('#end-game-button').show();
         $(this).hide();
         score = 0;
         timeLeft = 30;
@@ -45,9 +59,12 @@ $(document).ready(function() {
             generateSlug();
             updateTimer();
         }, 1000);
+
+        // Play the background music---------------------------------------------------------------------------
+        //$('#background-music')[0].play();
     }
 
-    // Function to update the timer
+    // Function to update the timer ---------------------------------------------------------------------------
     function updateTimer() {
         if (--timeLeft <= 0) {
             endGame();
@@ -56,34 +73,33 @@ $(document).ready(function() {
         updateUI();
     }
 
-    // Function to end the game
-function endGame() {
+ // Function to end the game---------------------------------------------------------------------------
+ function endGame() {
     clearInterval(gameInterval);
-    $('.slug').remove(); // Remove all slugs from the game area
-    displayScoreResult(); // Display the score result
-    $('#end-game-button').hide(); // Hide the end game button when the game ends
-    if ($('#reset-button').length === 0) {
-        gameArea.append('<div>GAME OVER</div><button id="reset-button">Play Again</button>');
-    }
-    $('#reset-button').click(function() {
-        createGameUI();
-        $(this).remove(); // Remove the reset button after clicking
-    });
+    $('.slug').remove();
+    displayScoreResult();
+    $('#end-game-button').hide();
+
+    // Stop the background music---------------------------------------------------------------------------
+    //$('#background-music')[0].pause();
+    //$('#background-music')[0].currentTime = 0;
+
+    // Select a random fun fact about banana slugs---------------------------------------------------------------------------
+    const randomFact = bananaSlugFacts[Math.floor(Math.random() * bananaSlugFacts.length)];
+    $('#fact-text').text(randomFact);
+
+    // Show the end screen with the fun fact---------------------------------------------------------------------------
+    $('#end-screen').show();
 }
-
-// Function to display the score result with animations
-function displayScoreResult() {
-    let scoreResult = $('<div id="score-result"></div>');
-    if (score > 10) {
-        scoreResult.html(`Congratulations! Your score is: ${score}<br><span class="party-animation">🎉🎊🥳</span>`);
-    } else {
-        scoreResult.html(`Your score is: ${score}<br><span class="giant-L">L</span>`);
+    // Function to display the score result---------------------------------------------------------------------------
+    function displayScoreResult() {
+        let scoreResult = $('<div id="score-result"></div>');
+        scoreResult.html(`Your score is: ${score}`);
+        gameArea.append(scoreResult);
     }
-    gameArea.append(scoreResult);
-}
 
 
-    // Function to update the score and timer UI
+    // Function to update the score and timer UI---------------------------------------------------------------------------
     function updateUI() {
         $('#score').text(score);
         let widthPercentage = (timeLeft / 30) * 100;
@@ -91,28 +107,72 @@ function displayScoreResult() {
         $('#time-left').css({ width: widthPercentage + '%', backgroundColor: color });
     }
 
-    // Function to generate and place a new slug on the screen
+    // Function to generate and place a new slug on the screen---------------------------------------------------------------------------
     function generateSlug() {
-        let slug = $('<div class="slug"></div>').css({
+        let randomChance = Math.random();
+        if (randomChance < 0.1) {
+            generateRedSlug();
+        } else if (randomChance < 0.2) { // 10% chance for a green slug
+            generateGreenSlug();
+        } else {
+            let slug = $('<div class="slug"></div>').css({
+                top: 0,
+                left: Math.floor(Math.random() * (gameArea.width() - 60)),
+                backgroundImage: 'url(Images/8bit-slug.png)' // Path to slug image
+            }).click(function() {
+                $(this).effect('explode', 500, function() { $(this).remove(); });
+                score++;
+                updateUI();
+            });
+
+            gameArea.append(slug);
+            moveSlug(slug);
+        }
+    }
+
+    // Function to generate and place a red slug---------------------------------------------------------------------------
+    function generateRedSlug() {
+        let redSlug = $('<div class="slug red-slug"></div>').css({
             top: 0,
             left: Math.floor(Math.random() * (gameArea.width() - 60)),
-            backgroundImage: 'url(Images/8bit-slug.png)' // Path to slug image
+            backgroundImage: 'url(Images/red-slug.png)'
+        }).click(function() {
+            endGame(); // End the game if a red slug is clicked
+        });
+
+        gameArea.append(redSlug);
+        moveSlug(redSlug);
+    }
+
+    // Function to generate and place a green slug---------------------------------------------------------------------------
+    function generateGreenSlug() {
+        let greenSlug = $('<div class="slug green-slug"></div>').css({
+            top: 0,
+            left: Math.floor(Math.random() * (gameArea.width() - 60)),
+            backgroundImage: 'url(Images/green-slug.png)'
         }).click(function() {
             $(this).effect('explode', 500, function() { $(this).remove(); });
-            score++;
+            score += 2; // Double points for green slug
             updateUI();
         });
 
-        gameArea.append(slug);
-        moveSlug(slug);
+        gameArea.append(greenSlug);
+        moveSlug(greenSlug, true); // Pass true to indicate it's a green slug
     }
 
-    // Function to move the slug down the screen
-    function moveSlug(slug) {
+    // Function to move the slug down the screen----------------------------------------------------------------------------
+    function moveSlug(slug, isGreenSlug = false) {
         let moveInterval = setInterval(() => {
             let currentTop = slug.position().top;
+            let speed = parseInt($('#speed-slider').val()); // Get current speed from slider
+            if (slug.hasClass('red-slug')) {
+                speed += 5; // Red slugs fall faster
+            } else if (isGreenSlug) {
+                speed *= 2; // Green slugs fall two times faster
+            }
+
             if (currentTop < gameArea.height() - 100) {
-                slug.css('top', currentTop + slugSpeed);
+                slug.css('top', currentTop + speed);
             } else {
                 slug.remove();
                 clearInterval(moveInterval);
@@ -122,9 +182,27 @@ function displayScoreResult() {
 
     createGameUI();
 
-    // Event listener for the end game button
+    // Event listener for the end game button-------------------------------------------
     $('#end-game-button').click(function() {
         endGame();
         $(this).hide();
     });
+
+    // Event listener for speed slider---------------------------------------------------------------------------
+    $('#speed-slider').on('input change', function() {
+        $('#speed-value').text($(this).val()); // Update the speed display
+    });
+
+    // Event listener for closing the end screen---------------------------------------------------------------------------
+    $('#close-end-screen').click(function() {
+        $('#end-screen').hide();
+    });
+
+    // Event listener for the play again button---------------------------------------------------------------------------
+    $('#play-again-button').click(function() {
+        $('#end-screen').hide();
+        createGameUI();
+    });
+
 });
+//------------------------------------------------------------------------------------------------------------------------------------------------------
